5647fc879e5c837f5aa4e96f992b9c64b6cfa801
cluster777 <42055002+cluster777@users.noreply.github.com>
2020-04-16 11:48:19 +0700

    betulin masalah2 kecil

--- tabel.sql
+++ tabel.sql
@@ -31,7 +31,7 @@ constraint fk_penyebut foreign key(id_penyebut)
 )
 
 create table Periode(
-id int identity(1,1),
+id int identity(1,1) primary key,
 nama varchar(50),
 create_date datetime,
 last_update datetime
@@ -39,20 +39,20 @@ last_update datetime
 
 
 create table Indikator_Periode(
-id_master int,
-id_periode int,
+id_master int unique,
+id_periode int unique,
 bobot float,
 constraint fk_master foreign key(id_master)
 	references MasterIndikator(id),
-constraint fk_master foreign key(id_master)
+constraint fk_periode foreign key(id_periode)
 	references Periode(id)
 
 )
 create table SatuanKerja(
 id int identity(1,1),
-id_satker as 'SK'++ RIGHT('00000000000000000000000000000' + CAST(ID AS VARCHAR(30)), 30) PERSISTED primary key,
+id_satker as 'SK'+ RIGHT('00000000000000000000000000000' + CAST(ID AS varCHAR(30)), 30) PERSISTED primary key,
 id_ins_satker int,
-id_induk_satker char(32),
+id_induk_satker varchar(32),
 nama varchar(50),
 email varchar(50),
 create_date datetime,
@@ -65,54 +65,48 @@ constraint fk_induk_satker foreign key(id_induk_satker)
 )
 
 create table Capaian_Unit(
-id_satker char(32),
+id_satker varchar(32),
 id_datadasar int,
 waktu datetime,
 capaian float,
 constraint fk_satker foreign key(id_satker)
 	References SatuanKerja(id_satker) on delete cascade,
-constraint fk_datadasar foreign key(id_satker)
-	References DataDasar(id_datadasar) on delete cascade
+constraint fk_datadasar foreign key(id_datadasar)
+	References DataDasar(id) on delete cascade
 )
 
 create table Indikator_SatuanKerja(
-id_periode year,
-id_master integer,
-id_satker char(32),
+id_periode int unique,
+id_master integer unique,
+id_satker varchar(32) unique,
 bobot float,
 target float,
 capaian float,
 last_update datetime,
-constraint fk_periode foreign key (id_periode) 
+constraint fk_periode1 foreign key (id_periode) 
 	references Indikator_Periode(id_periode)
-	on delete cascade
-	on update cascade, 
-constraint fk_master foreign key (id_master) 
+	, 
+constraint fk_master1 foreign key (id_master) 
 	references Indikator_Periode(id_master)
-	on delete cascade
-	on update cascade,
-constraint fk_satker foreign key (id_satker) 
+	,
+constraint fk_satker1 foreign key (id_satker) 
 	references SatuanKerja(id_satker)
-	on delete cascade
-	on update cascade,
+	
 )
 
 create table Indikator_SatuanKerja_Log(
-id_periode year,
+id_periode int,
 id_master integer,
-id_satker char(32),
+id_satker varchar(32),
 capaian float,
 create_date datetime,
-constraint fk_periode foreign key (id_periode) 
+constraint fk_periode2 foreign key (id_periode) 
 	references Indikator_SatuanKerja(id_periode)
-	on delete cascade
-	on update cascade, 
-constraint fk_master foreign key (id_master) 
+	, 
+constraint fk_master2 foreign key (id_master) 
 	references Indikator_SatuanKerja(id_master)
-	on delete cascade
-	on update cascade,
-constraint fk_satker foreign key (id_satker) 
+	,
+constraint fk_satker2 foreign key (id_satker) 
 	references Indikator_SatuanKerja(id_satker)
-	on delete cascade
-	on update cascade,
+	
 )
